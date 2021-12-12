d069616678a64dba7dc3e3f31a89b813e7a49e16
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-12 22:38:48 +0000

    Add 2021 day 5

---
+++ src/Y2021/Day5/LineSegment.cs
@@ -0,0 +1,53 @@
+namespace AdventOfCode.Y2021.Day5
+{
+    public struct LineSegment
+    {
+        public LineSegment(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Point Start { get; }
+
+        public Point End { get; }
+
+        public bool IsHorizontal => Start.Y == End.Y;
+
+        public bool IsVertical => Start.X == End.X;
+
+        public IEnumerable<Point> GetPoints()
+        {
+            var x = Start.X;
+            var y = Start.Y;
+
+            var dx = End.X.CompareTo(Start.X);
+            var dy = End.Y.CompareTo(Start.Y);
+
+            var points = new List<Point>();
+            while (x != End.X || y != End.Y)
+            {
+                points.Add(new Point(x, y));
+                x += dx;
+                y += dy;
+            }
+            points.Add(End);
+
+            return points;
+        }
+
+        public static LineSegment Parse(string line)
+        {
+            // string containing x1,y1 -> x2,y2
+            var parts = line.Split(' ');
+            var start = Point.Parse(parts[0]);
+            var end = Point.Parse(parts[2]);
+            return new LineSegment(start, end);
+        }
+
+        public override string ToString()
+        {
+            return $"{Start} -> {End}";
+        }
+    }
+}
---
+++ src/Y2021/Day5/Point.cs
@@ -0,0 +1,55 @@
+namespace AdventOfCode.Y2021.Day5;
+
+public struct Point
+{
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+
+    public int Y { get; }
+
+    public static Point Parse(string value)
+    {
+        var parts = value.Split(',');
+        return new Point(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+
+    public static bool operator==(Point x, Point y)
+    {
+        return x.X == y.X && x.Y == y.Y;
+    }
+
+    public static bool operator !=(Point x, Point y)
+    {
+        return !(x == y);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if(obj is not Point point)
+        {
+            return false;
+        }
+
+        return this == point;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public override string ToString()
+    {
+        return $"{X},{Y}";
+    }
+}
