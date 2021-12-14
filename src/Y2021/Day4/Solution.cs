ca5f1b58f5db98ac56f79dd641fcb184b5bcf64f
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-14 21:34:12 +0000

    Add day 8 solution

--- src/Y2021/Day4/Solution.cs
+++ src/Y2021/Day4/Solution.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace AdventOfCode.Y2021.Day4;
 
-namespace AdventOfCode.Y2021.Day4;
 public class Solution : IPuzzleSolution
 {
     public long Part1(StreamReader reader)
--- src/Y2021/Day8/Solution.cs
+++ src/Y2021/Day8/Solution.cs
@@ -4,11 +4,107 @@ public class Solution : IPuzzleSolution
 {
     public long Part1(StreamReader reader)
     {
-        throw new NotImplementedException();
+        var total = 0;
+
+        while (reader.TryReadLine(out string line))
+        {
+            total += line
+                .Split('|')
+                .ElementAt(1)
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(signal => signal.Length)
+                .Where(length => new int[] { 2, 4, 3, 7 }.Contains(length))
+                .Count();
+        }
+
+        return total;
     }
 
     public long Part2(StreamReader reader)
     {
-        throw new NotImplementedException();
+        var total = 0;
+
+        while (reader.TryReadLine(out string line))
+        {
+            var patterns = line
+                .Split('|')
+                .SelectMany(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                .Select(x => new string(x.OrderBy(c => c).ToArray()))
+                .ToList();
+
+            var map = GetSegmentMap(patterns);
+
+            var output = patterns
+               .Skip(patterns.Count - 4)
+               .Take(4)
+               .Select(x => map[x])
+               .ToList();
+
+            var value = (1000 * output[0]) + (100 * output[1]) + (10 * output[2]) + output[3];
+            total += value;
+        }
+
+        return total;
+    }
+
+    private static Dictionary<string,int> GetSegmentMap(List<string> patterns)
+    {
+        var patternsByLength = patterns
+            .OrderBy(x => x.Length)
+            .ToList();
+
+        var map = new string[10];
+
+        foreach (var pattern in patternsByLength)
+        {
+            switch (pattern.Length)
+            {
+                case 2:
+                    map[1] = pattern;
+                    break;
+                case 3:
+                    map[7] = pattern;
+                    break;
+                case 4:
+                    map[4] = pattern;
+                    break;
+                case 5: // 2, 3 or 5
+                    if (pattern.Intersect(map[7]).Count() == 3)
+                    {
+                        map[3] = pattern;
+                    }
+                    else if (pattern.Intersect(map[4]).Count() == 2)
+                    {
+                        map[2] = pattern;
+                    }
+                    else // 5 intersect 4 = 3
+                    {
+                        map[5] = pattern;
+                    }
+                    break;
+                case 6: // 0, 6, or 9
+                    if (pattern.Intersect(map[4]).Count() == 4)
+                    {
+                        map[9] = pattern;
+                    }
+                    else if (pattern.Intersect(map[7]).Count() == 3)
+                    {
+                        map[0] = pattern;
+                    }
+                    else
+                    {
+                        map[6] = pattern;
+                    }
+                    break;
+                case 7:
+                    map[8] = pattern;
+                    break;
+            }
+        }
+
+        return map
+            .Select((x, i) => new { Value = i, Pattern = x })
+            //.Where(x => x.Pattern != null)
+            .ToDictionary(x => x.Pattern, x => x.Value);
     }
 }
