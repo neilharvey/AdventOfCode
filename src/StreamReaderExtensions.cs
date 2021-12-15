81a5e0e9e61f5e9f603a1f0f9a642368ab10e72e
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-15 23:16:28 +0000

    Add day 9

--- src/StreamReaderExtensions.cs
+++ src/StreamReaderExtensions.cs
@@ -7,4 +7,24 @@ public static class StreamReaderExtensions
         line = reader.ReadLine()!;
         return line != null;
     }
+
+    public static int[,] ReadMatrix(this StreamReader reader)
+    {
+        var lines = new List<string>();
+        while (reader.TryReadLine(out string line))
+        {
+            lines.Add(line);
+        }
+
+        var matrix = new int[lines[0].Length, lines.Count];
+        for (var row = 0; row < lines.Count; row++)
+        {
+            for (var col = 0; col < lines[row].Length; col++)
+            {
+                matrix[col, row] = lines[row][col] - '0';
+            }
+        }
+
+        return matrix;
+    }
 }
--- src/Y2021/Day9/Solution.cs
+++ src/Y2021/Day9/Solution.cs
@@ -2,39 +2,87 @@
 
 public class Solution : IPuzzleSolution
 {
+    private delegate long Calculation(int[,] heightmap, List<(int x, int y)> lowpoints);
+
     public long Part1(StreamReader reader)
     {
-        string prev = null;
-        string current = reader.ReadLine();
-        string next = reader.ReadLine();
+        var heightmap = reader.ReadMatrix();
+        var lowpoints = FindLowPoints(heightmap);
 
-        var risk = 0;
-
-        while (current != null)
-        {
-            for (var i = 0; i < current.Length; i++)
-            {
-                var up = prev == null || current[i] < prev[i];
-                var down = next == null || current[i] < next[i];
-                var left = i == 0 || current[i] < current[i - 1];
-                var right = i == current.Length - 1 || current[i] < current[i + 1];
-
-                if (up && down && left && right)
-                {
-                    risk += 1 + (current[i] - '0');
-                }
-            }
-
-            prev = current;
-            current = next;
-            next = reader.ReadLine();
-        }
-
-        return risk;
+        return lowpoints
+            .Select(t => 1 + heightmap[t.x, t.y])
+            .Sum();
     }
 
     public long Part2(StreamReader reader)
     {
-        return 0;
+        var heightmap = reader.ReadMatrix();
+        var lowpoints = FindLowPoints(heightmap);
+
+        var basinSizes = new List<int>();
+        var basins = new int[heightmap.GetLength(0), heightmap.GetLength(1)];
+
+        foreach (var lowpoint in lowpoints)
+        {
+            var basinSize = 0;
+            var queue = new Queue<(int, int)>();
+            queue.Enqueue(lowpoint);
+            while (queue.Count > 0)
+            {
+                var (x, y) = queue.Dequeue();
+                if (heightmap[x, y] < 9 && basins[x, y] == 0)
+                {
+                    basinSize++;
+                    basins[x, y] = 1;
+                    if (x > 0)
+                    {
+                        queue.Enqueue((x - 1, y));
+                    }
+                    if (x < heightmap.GetLength(0) - 1)
+                    {
+                        queue.Enqueue((x + 1, y));
+                    }
+                    if (y > 0)
+                    {
+                        queue.Enqueue((x, y - 1));
+                    }
+                    if (y < heightmap.GetLength(1) - 1)
+                    {
+                        queue.Enqueue((x, y + 1));
+                    }
+                }
+            }
+
+            basinSizes.Add(basinSize);
+        }
+
+        var largest = basinSizes
+            .OrderByDescending(x => x)
+            .ToList();
+
+        return largest[0] * largest[1] * largest[2];
+    }
+
+    private static List<(int x, int y)> FindLowPoints(int[,] heightmap)
+    {
+        var lowpoints = new List<(int, int)>();
+
+        for (var x = 0; x < heightmap.GetLength(0); x++)
+        {
+            for (var y = 0; y < heightmap.GetLength(1); y++)
+            {
+                var up = x == 0 || heightmap[x, y] < heightmap[x - 1, y];
+                var down = x == heightmap.GetLength(0) - 1 || heightmap[x, y] < heightmap[x + 1, y];
+                var left = y == 0 || heightmap[x, y] < heightmap[x, y - 1];
+                var right = y == heightmap.GetLength(1) - 1 || heightmap[x, y] < heightmap[x, y + 1];
+
+                if (up && down && left && right)
+                {
+                    lowpoints.Add((x, y));
+                }
+            }
+        }
+
+        return lowpoints;
     }
 }
