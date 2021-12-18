6d085e149571ff0467d6a5dd2871a64cf01e1ee0
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-18 22:52:08 +0000

    Refactored to improve lookup performance of caves already seen

---
+++ src/Y2021/Day12/Path.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode.Y2021.Day12
+{
+    public class Path
+    {
+        private readonly List<Cave> _caves = new();
+        private readonly Dictionary<string, int> _timesVisited = new();
+
+        public Path() { }
+
+        public Path(Path path)
+        {
+            _caves = new List<Cave>(path._caves);
+            _timesVisited = new Dictionary<string, int>(path._timesVisited);
+            AnySmallCaveVisitedTwice = path.AnySmallCaveVisitedTwice;
+        }
+
+        public bool AnySmallCaveVisitedTwice { get; private set; }
+
+        public void Add(Cave cave)
+        {
+            _caves.Add(cave);
+            if (Contains(cave) && cave.IsSmall)
+            {
+                AnySmallCaveVisitedTwice = true;
+            }
+
+            SetVisited(cave);
+        }
+
+        public bool Contains(Cave cave)
+        {
+            return _timesVisited.ContainsKey(cave.Label);
+        }
+
+        private void SetVisited(Cave cave)
+        {
+            if (_timesVisited.ContainsKey(cave.Label))
+            {
+                _timesVisited[cave.Label]++;
+            }
+            else
+            {
+                _timesVisited.Add(cave.Label, 1);  
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", _caves);
+        }
+    }
+}
