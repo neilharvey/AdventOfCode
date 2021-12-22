c3835c246f9d5f5683aa3515bc844c7f32865608
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-22 16:30:32 +0000

    Add day 17  solution

--- src/Y2021/Day17/Solution.cs
+++ src/Y2021/Day17/Solution.cs
@@ -1,15 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2021.Day17
 {
     public class Solution : IPuzzleSolution
     {
         public long Part1(StreamReader reader)
+        {
+            var target = GetTargetArea(reader);
+            var velocityY = Math.Abs(target.MinY) - 1;
+            var maxHeight = (velocityY * (velocityY + 1)) / 2;
+            return maxHeight;
+        }
+
+        public long Part2(StreamReader reader)
+        {
+            var target = GetTargetArea(reader);
+            var vminX = (int)(Math.Sqrt(2 * target.MinX));
+            var vmaxX = target.MaxX;
+            var vminY = target.MinY;
+            var vmaxY = -1 * target.MinY;
+
+            var velocities = new List<(int x, int y)>();
+            for (var x = vminX; x <= vmaxX; x++)
+            {
+                for (var y = vminY; y <= vmaxY; y++)
+                {
+                    if (IsHit(x, y, target))
+                    {
+                        velocities.Add((x, y));
+                    }
+                }
+            }
+
+            return velocities.Count;
+        }
+
+        private static bool IsHit(int velocityX, int velocityY, TargetArea target)
+        {
+            (int x, int y) position = (0, 0);
+
+            while (position.x <= target.MaxX && position.y >= target.MinY)
+            {
+                position = (position.x + velocityX, position.y + velocityY);
+
+                velocityY--;
+                if (velocityX > 0)
+                {
+                    velocityX--;
+                }
+
+                if (position.x >= target.MinX && position.x <= target.MaxX && position.y >= target.MinY && position.y <= target.MaxY)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static TargetArea GetTargetArea(StreamReader reader)
         {
             var re = new Regex(@"x=(\d+)..(\d+), y=([-]\d+)..([-]\d+)");
             var line = reader.ReadLine();
@@ -19,16 +68,7 @@ namespace AdventOfCode.Y2021.Day17
             var minY = Convert.ToInt32(match.Groups[3].Value);
             var maxY = Convert.ToInt32(match.Groups[4].Value);
 
-            var velocityY = Math.Abs(minY) - 1;
-            var maxHeight = (velocityY * (velocityY + 1)) / 2;
-            return maxHeight;
+            return new TargetArea(minX, maxX, minY, maxY);
         }
-
-        public long Part2(StreamReader reader)
-        {
-            throw new NotImplementedException();
-        }
-
-
     }
 }
---
+++ src/Y2021/Day17/TargetArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdventOfCode.Y2021.Day17;
+
+public readonly record struct TargetArea
+{
+    public TargetArea(int minX, int maxX, int minY, int maxY)
+    {
+        MinX = minX;
+        MinY = minY;
+        MaxX = maxX;
+        MaxY = maxY;
+    }
+
+    public int MinX { get; }
+
+    public int MaxX { get; }
+
+    public int MinY { get; }
+
+    public int MaxY { get; }
+}
