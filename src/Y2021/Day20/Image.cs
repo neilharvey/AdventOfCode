1316e50c3d952b8cfb913c914be310f3f8e5683c
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-12-31 20:33:53 +0000

    Init day 20

---
+++ src/Y2021/Day20/Image.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Y2021.Day20
+{
+    public class Image
+    {
+        private readonly Dictionary<(int x, int y), bool> _pixels = new();
+
+        public bool this[int x, int y]
+        {
+            get { return _pixels[(x, y)]; }
+            set { 
+                _pixels[(x,y)] = value;
+            }
+        }
+    }
+}
---
+++ src/Y2021/Day20/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Y2021.Day20
+{
+    public class Solution : IPuzzleSolution
+    {
+        public long Part1(StreamReader reader)
+        {
+            var algorithm = reader.ReadLine();
+            _ = reader.ReadLine(); // blank
+
+        }
+
+        public long Part2(StreamReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
