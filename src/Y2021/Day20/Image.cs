f13e371dd9e0cd3e8368f0b327b4c65fe2f3c350
neilharvey <neil.harvey@hotmail.co.uk>
2021-12-31 22:13:29 +0000

    Add initial attempt for day 20

--- src/Y2021/Day20/Image.cs
+++ src/Y2021/Day20/Image.cs
@@ -1,15 +1,50 @@
-﻿namespace AdventOfCode.Y2021.Day20
+﻿using System.Text;
+
+namespace AdventOfCode.Y2021.Day20
 {
     public class Image
     {
         private readonly Dictionary<(int x, int y), bool> _pixels = new();
 
+        public int MinX => _pixels.Min(p => p.Key.x);
+
+        public int MaxX => _pixels.Max(p => p.Key.y);
+
+        public int MinY => _pixels.Min(p => p.Key.y);
+
+        public int MaxY => _pixels.Max(p => p.Key.y);
+
         public bool this[int x, int y]
         {
-            get { return _pixels[(x, y)]; }
-            set { 
-                _pixels[(x,y)] = value;
+            get
+            {
+                if (_pixels.TryGetValue((x, y), out bool value))
+                {
+                    return value;
+                }
+                else
+                {
+                    return false;
+                }
             }
+            set
+            {
+                _pixels[(x, y)] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var y = _pixels.Min(p => p.Key.y); y <= _pixels.Max(p => p.Key.y); y++)
+            {
+                for (var x = _pixels.Min(p => p.Key.x); x <= _pixels.Max(p => p.Key.x); x++)
+                {
+                    sb.Append(_pixels[(x, y)] ? '#' : '.');
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
         }
     }
 }
--- src/Y2021/Day20/Solution.cs
+++ src/Y2021/Day20/Solution.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace AdventOfCode.Y2021.Day20
 {
@@ -13,6 +9,91 @@ namespace AdventOfCode.Y2021.Day20
             var algorithm = reader.ReadLine();
             _ = reader.ReadLine(); // blank
 
+            var image = ReadImage(reader);
+        //    Console.WriteLine("--- Initial ---");
+          //  PrintImage(image);
+            image = EnhanceImage(image, algorithm);
+    //        Console.WriteLine("--- Enhancement 1 ---");
+      //      PrintImage(image);
+            image = EnhanceImage(image, algorithm);
+//            Console.WriteLine("--- Enhancement 2 ---");
+  //          PrintImage(image);
+
+            var litPixels = 0;
+            for (var x = image.MinX; x <= image.MaxX; x++)
+            {
+                for (var y = image.MinY; y <= image.MaxY; y++)
+                {
+                    if (image[x, y])
+                    {
+                        litPixels++;
+                    }
+                }
+            }
+
+            return litPixels;
+        }
+
+        private static void PrintImage(Image image)
+        {
+            for (var y = image.MinY; y <= image.MaxY; y++)
+            {
+                for (var x = image.MinX; x <= image.MaxX; x++)
+                {
+                    Console.Write(image[x, y] ? '#' : '.');
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static Image ReadImage(StreamReader reader)
+        {
+            var image = new Image();
+            var y = 0;
+
+            while (reader.TryReadLine(out string line))
+            {
+                for (var x = 0; x < line.Length; x++)
+                {
+                    image[x, y] = line[x] == '#';
+                }
+
+                y++;
+            }
+
+            return image;
+        }
+
+        private static Image EnhanceImage(Image image, string algorithm)
+        {
+            var enhanced = new Image();
+
+            for (var x = image.MinX; x <= image.MaxX; x++)
+            {
+                for (var y = image.MinY; y <= image.MaxY; y++)
+                {
+                    var enhancementIndex = GetEnhancementIndex(image, x, y);
+                    enhanced[x, y] = algorithm[enhancementIndex] == '#';
+                }
+            }
+
+            return enhanced;
+        }
+
+        private static int GetEnhancementIndex(Image image, int x, int y)
+        {
+            var sb = new StringBuilder();
+            sb.Append(image[x - 1, y - 1] ? "1" : "0");
+            sb.Append(image[x, y - 1] ? "1" : "0");
+            sb.Append(image[x + 1, y - 1] ? "1" : "0");
+            sb.Append(image[x - 1, y] ? "1" : "0");
+            sb.Append(image[x, y] ? "1" : "0");
+            sb.Append(image[x + 1, y] ? "1" : "0");
+            sb.Append(image[x - 1, y + 1] ? "1" : "0");
+            sb.Append(image[x, y + 1] ? "1" : "0");
+            sb.Append(image[x + 1, y + 1] ? "1" : "0");
+            return Convert.ToInt32(sb.ToString(), 2);
         }
 
         public long Part2(StreamReader reader)
