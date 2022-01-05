37e72ac80d548a8b7c9563418bfe73225398c5b5
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-01-05 22:54:14 +0000

    Init days 19, 22, 23

---
+++ src/Y2021/Day19/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Y2021.Day19
+{
+    public class Solution : IPuzzleSolution
+    {
+        public long Part1(StreamReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Part2(StreamReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
