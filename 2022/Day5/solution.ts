2afe1102bbc9142a950c6a054f3b65016d82ccc1
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-12-05 19:02:28 +0000

    Add day 5 part 2 solution

--- 2022/Day5/solution.ts
+++ 2022/Day5/solution.ts
@@ -1,45 +1,48 @@
 import { readInput } from '../aoc';
 const input = readInput();
-
 const stack_count = (input[0].length + 1) / 4;
-const move_regex = /.+\*.+/;
 
-console.log(`Stacks: ${stack_count}`);
-let stacks: Array<Array<string>> = Array(stack_count);
-for(let i=0; i<stack_count; i++) {
-    stacks[i] = [];
-}
+function supply_stacks(input: string[], move_multiple: boolean) {
 
-input.forEach(line => {
+    let stacks: Array<Array<string>> = Array(stack_count);
+    for (let i = 0; i < stack_count; i++) {
+        stacks[i] = [];
+    }
 
-    // crates
-    if (line.indexOf("[") !== -1) {
-        for (let i = 0; i < stack_count; i++) {
-            var crate = line[1 + (i * 4)];
-            if(crate !== ' ') {
-                stacks[i].push(crate);
+    input.forEach(line => {
+
+        // crates
+        if (line.indexOf("[") !== -1) {
+            for (let i = 0; i < stack_count; i++) {
+                var crate = line[1 + (i * 4)];
+                if (crate !== ' ') {
+                    stacks[i].push(crate);
+                }
             }
         }
-    }
 
-    // instructions
-    if (line.startsWith("move")) {
-        const parts = line.split(" ");
-        const quantity = Number(parts[1]);
-        const from = Number(parts[3]);
-        const to = Number(parts[5]);
+        // instructions
+        if (line.startsWith("move")) {
+            const parts = line.split(" ");
+            const quantity = Number(parts[1]);
+            const from = Number(parts[3]);
+            const to = Number(parts[5]);
 
-        for(let i=0; i< quantity; i++) {
-            let crate = <string>stacks[from - 1].shift();
-            stacks[to - 1].unshift(crate);
+            let crates_to_move: string[] = [];
+            for (let i = 0; i < quantity; i++) {
+                let crate = <string>stacks[from - 1].shift();
+                crates_to_move.unshift(crate);
+            }
+
+            for (let i = 0; i < quantity; i++) {
+                let crate = move_multiple ? crates_to_move.shift() : crates_to_move.pop();
+                stacks[to - 1].unshift(<string>crate);
+            }
         }
-    }
-});
+    });
 
-let answer = "";
+    return stacks.reduce<string>((a, c) => a + c[0], "");
+}
 
-stacks.forEach(stack => {
-    answer += stack[0];
-})
-
-console.log(answer);
+console.log(`Part One: ${supply_stacks(input, false)}`);
+console.log(`Part Two: ${supply_stacks(input, true)}`);
